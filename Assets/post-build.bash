f20c915cbdbc23a2eab241f14b99a89443d769f2
Brojas37 <elijahrojas37@gmail.com>
2024-06-29 14:39:03 -0700

    file containing "frameworks"
    set build to 8
    
    edited postbuild script to set "always_embed_swift_standard_libraries" to no

--- Assets/post-build.bash
+++ Assets/post-build.bash
@@ -5,6 +5,12 @@ path="$WORKSPACE/.build/last/$TARGET_NAME/build.ipa"
 echo "WORKSPACE: $WORKSPACE"
 echo "TARGET_NAME: $TARGET_NAME"
 echo "IPA Path: $path"
+
+xcodeproj_path="$WORKSPACE/Unity-iPhone.xcodeproj/project.pbxproj"
+if grep -q "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES" "$xcodeproj_path"; then
+    sed -i '' 's/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = YES;/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = NO;/g' "$xcodeproj_path"
+fi
+
 if xcrun altool --upload-app -t ios -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
     echo "Upload IPA to Appstore Connect finished with success"
 else
