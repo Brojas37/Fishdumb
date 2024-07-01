370c5d79df0931ee1c25f8aaa5459a864daa889c
Brojas37 <elijahrojas37@gmail.com>
2024-07-01 10:49:25 -0700

    trying to fix frameworks error 2
    changed build to 10
    
    another shot at fixing frameworks error

--- Assets/post-build.bash
+++ Assets/post-build.bash
@@ -6,10 +6,7 @@ echo "WORKSPACE: $WORKSPACE"
 echo "TARGET_NAME: $TARGET_NAME"
 echo "IPA Path: $path"
 
-xcodeproj_path="$WORKSPACE/Unity-iPhone.xcodeproj/project.pbxproj"
-if grep -q "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES" "$xcodeproj_path"; then
-    sed -i '' 's/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = YES;/ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES = NO;/g' "$xcodeproj_path"
-fi
+export OTHER_SWIFT_FLAGS="-Xfrontend -embed-bitcode=no"
 
 if xcrun altool --upload-app -t ios -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
     echo "Upload IPA to Appstore Connect finished with success"
